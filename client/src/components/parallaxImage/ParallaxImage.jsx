a49eab8a8da631071dac4be8e053725c643bd0cf
Amar Vadhia <amar.vadhia@gmail.com>
2019-11-20 20:07:23 -0800

    fix styling for parallax images and logo animation

--- client/src/components/parallaxImage/ParallaxImage.jsx
+++ client/src/components/parallaxImage/ParallaxImage.jsx
@@ -4,7 +4,8 @@ import styles from './ParallaxImage.css';
 const ParallaxImage = (props) => {
   return(
     <div className={styles.parallaxBackgroundImage}>
-      <h1>Build your dreams</h1>
+      {/* <h1>Build your dreams</h1> */}
+      <h1>BUILD YOUR DREAMS</h1>
     </div>
   )
 }
